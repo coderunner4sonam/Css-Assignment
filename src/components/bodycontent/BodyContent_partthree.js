a09704a6b2d5eaf2d763a9c452eddd82845460fe
Sonam <sonammurarkar41@gmail.com>
2023-01-16 19:49:44 +0530

    css done

--- src/components/bodycontent/BodyContent_partthree.js
+++ src/components/bodycontent/BodyContent_partthree.js
@@ -23,7 +23,6 @@ function BodyContent_partthree() {
       }
       const bodyContent_partthree={
         height:"45vh",
-       
         display:"flex",
         justifyContent:"space-around",
         marginTop:"18px",
@@ -41,16 +40,24 @@ function BodyContent_partthree() {
     const bodyContent_partthree_inner_inner={
       width:"65px",
       height:"65px",
-      borderRadius:"45%",
+      borderRadius:"50%",
       marginTop:"50px",
       marginLeft:"120px"
     }
     const parent_background={
-      backgroundImage: `url(${Mask})`,
-      borderRadius:"8%",
-      width:"320px",
-      height:"200px"
+      paddingTop:"30px",
+      paddingRight:"20px",
+      paddingBottom:"40px",
+      paddingLeft:"20px",
+      backgroundColor:"white",
+      marginTop:"22px",
+      borderRadius:"20px",
+      display:"flex",
+      flexDirection:"column",
+      alignItems: 'center',
+      justifyContent:"space-between"
     }
+
     const parthtree_label_heading={
       marginLeft:"26%"
     }
@@ -63,7 +70,12 @@ function BodyContent_partthree() {
     const parthtree_label_para3={
       marginLeft:"26%"
     }
-
+    const bodyContent_partthree_inner_inner2={
+      width:"65px",
+      height:"65px",
+      borderRadius:"50%",
+     
+    }
   return (
     <>
         <section >
@@ -79,11 +91,13 @@ function BodyContent_partthree() {
           <p className='parthtree_label_para' style={parthtree_label_para}>Executive Chef,USA</p>
         </div>
 
-        <div className='parent_background' style={parent_background}>
-          
-          <img src={Menpic} alt="Notebook" className='bodyContent_partthree_inner_inner' style={bodyContent_partthree_inner_inner}/>
-            <h4 className='parthtree_label_heading' style={parthtree_label_heading}>Robert Dowery Jr</h4>
-            <p className='parthtree_label_para' style={parthtree_label_para2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+        <div >
+          <div className='parent_background' style={parent_background}>
+          <img src={Menpic} alt="Notebook" className='bodyContent_partthree_inner_inner2' style={bodyContent_partthree_inner_inner2}/>
+            <h4 >Robert Dowery Jr</h4>
+            <p >Lorem Ipsum is simply dummy text of the printing  </p>
+            <p >and typesetting industry. </p>
+          </div>
         </div>
         
         <div className='bodyContent_partthree_inner_one' style={bodyContent_partthree_inner_one}>
